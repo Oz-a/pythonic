ab96a01aefc9ceed0aed4431ee436920b2a2d40a
Ozan Cetin <cetin.ozanahmet@gmail.com>
2022-07-02 23:04:21 -0400

    played with classes a little

---
+++ classes.py
@@ -0,0 +1,14 @@
+class University:
+	def __init__(self, name, endowment, students):
+		self.name = name
+		self.endowment = endowment
+		self.students = students
+
+	def endowment_per_student(self):
+		return self.endowment//self.students
+
+AU = University(name='American University', endowment=884305000, students=14318)
+Harvard = University(name='Harvard University', endowment=53200000000, students=22947)
+
+print(f'{AU.name} has ${AU.endowment_per_student()} of endowment money per student.')
+print(f'{Harvard.name} has ${Harvard.endowment_per_student()} of endowment money per student.')
